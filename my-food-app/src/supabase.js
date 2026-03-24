c2d608d8e02d3248fa654cca94179c3255930793
lnolom004 <lnolom004@example.com>
2026-03-25 00:44:12 +0700

    final fix for vercel

--- my-food-app/src/supabase.js
+++ my-food-app/src/supabase.js
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://your-url.supabase.co'
 const supabaseKey = 'your-anon-key'
 
-// ให้เหลือแค่บรรทัดนี้ชุดเดียวพอครับ
+// 
 export const supabase = createClient(supabaseUrl, supabaseKey) 
