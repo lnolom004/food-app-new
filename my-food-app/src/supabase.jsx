f4c780dbdaa44fb4d6ec6c78ddc78451008d1cce
lnolom004 <lnolom004@example.com>
2026-03-29 02:41:16 +0700

    update admin ui and fix supabase config

--- my-food-app/src/supabase.jsx
+++ my-food-app/src/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 🌟 เรียกใช้ผ่านตัวแปรนี้เพื่อให้รันได้ทั้งในเครื่องและ Vercel
+// 🌟 เปลี่ยนมาใช้รูปแบบนี้เพื่อให้รันได้ทั้งในเครื่องและ Vercel
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
